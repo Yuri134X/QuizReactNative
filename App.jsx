45dfbba3cb22d029e429dd43a119959bb243be4b
yuri134x <yurifilipe2004@gmail.com>
2024-04-06 11:02:26 -0300

    add scrollView in layout

--- App.jsx
+++ App.jsx
@@ -1,11 +1,22 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { ScrollView, StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>jackson uuuoooooooooooooou</Text>
-      <StatusBar style="auto" />
+      <view style= {{flex: 1, backgroundColor: 'blue', width: '100%'}}>
+        <text>Header</text>
+      </view>
+
+     
+      <ScrollView style={{flex:8, backgroundColor: 'green'}}>
+        <text> Lorem, ipsum dolor sit amet consectetur adipisicing elit. Optio ducimus dignissimos vero dolores temporibus consequuntur modi nemo repudiandae autem, dolorem quos! Blanditiis minima nobis modi culpa officiis, iusto veritatis dolorem!</text>
+        </ScrollView>
+        
+      <view style= {{flex: 1, backgroundColor: 'red', width: '100%'}}>
+      <text> footer</text>
+      </view>
+
     </View>
 
     
@@ -18,5 +29,6 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    textAlign: 'center'
   },
 });
