ec2a2f7056fa1fa68ddff098d75d4c909f3664f4
yuri134x <yurifilipe2004@gmail.com>
2024-04-09 10:30:20 -0300

    ADD NavigationContainer on code

--- App.jsx
+++ App.jsx
@@ -1,25 +1,25 @@
 import { StatusBar } from 'expo-status-bar';
 import { ScrollView, StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/nactive';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <view style= {{flex: 1, backgroundColor: 'blue', width: '100%'}}>
+
+      <view  style={styles.header}>
         <text>Header</text>
       </view>
 
-     
-      <ScrollView style={{flex:8, backgroundColor: 'green'}}>
+      <ScrollView style={styles.main}>
         <text> Lorem, ipsum dolor sit amet consectetur adipisicing elit. Optio ducimus dignissimos vero dolores temporibus consequuntur modi nemo repudiandae autem, dolorem quos! Blanditiis minima nobis modi culpa officiis, iusto veritatis dolorem!</text>
         </ScrollView>
         
-      <view style= {{flex: 1, backgroundColor: 'red', width: '100%'}}>
-      <text> footer</text>
+      <view style={styles.footer}>
+      <text > footer</text>""
       </view>
 
     </View>
 
-    
   );
 }
 
@@ -31,4 +31,21 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     textAlign: 'center'
   },
+  header: {
+    flex: 0.2,
+     backgroundColor: 'blue',
+      width: '100%'
+    },
+    main: {
+      flex: 1, 
+      backgroundColor: 'green',
+      width: '100%'
+    },
+
+      footer: {
+        flex: 0.1,
+         backgroundColor: 'red',
+          width: '100%'}
+
+          
 });
